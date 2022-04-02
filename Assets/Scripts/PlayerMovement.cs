8df739089816b7d225f4e2cd678c1f809a2ebde9
BrainzCode5 <99809473+BrainzCode5@users.noreply.github.com>
2022-04-02 14:08:07 +0300

    Movement Done

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(x * speed, y * speed).normalized;
+    }
+}
