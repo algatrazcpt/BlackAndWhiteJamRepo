8cd3a73ddbb01135d5d311dccb046fdce2121be9
algatrazcpt <53114980+algatrazcpt@users.noreply.github.com>
2022-04-03 09:34:04 +0300

    Main Menu added

---
+++ Assets/Scripts/MainMenuUiController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MainMenuUiController : MonoBehaviour
+{
+   // public Button startButton;
+   // public Button settingsButton;
+    private void Start()
+    {
+    }
+    public void StartNewGame()
+    {
+
+    }
+    public void SettingsMenuShow()
+    {
+
+    }
+}
