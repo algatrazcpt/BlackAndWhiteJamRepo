e131d27823ad941a5e3250e8f6d9d73cd71d4f43
algatrazcpt <53114980+algatrazcpt@users.noreply.github.com>
2022-04-03 16:28:14 +0300

    canvas fixed

---
+++ Assets/Scripts/EventSystemFix.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventSystemFix : MonoBehaviour
+{
+    public static EventSystemFix Instance;
+    private void Awake()
+    {
+
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
